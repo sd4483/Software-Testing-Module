8c4df8ed06c68aa34f219cb5d7dcdfdcb1971729
sd4483 <sudheer.paturi@hotmail.com>
2023-02-20 13:31:29 +0000

    Synced with eclipse package

---
+++ Lab_01/Task04.java
@@ -0,0 +1,40 @@
+package Lab_01;
+
+public class Task04 {
+	public static void main(String[] args) {
+	       addOld(-10,10);
+	       addNew(-10,10);
+	       addNew(0,-10);
+	    }
+	    
+		public static void addOld(int x, int y) {
+	        int sum = y;
+	        while (x != 0){
+	            if (x < 0) {
+	                sum = sum - 1;
+	                x = x + 1;
+	            }
+	            else {
+	                sum = sum + 1;
+	                x = x - 1;
+	            }
+	        }
+	        System.out.println(sum);
+	    }
+	    
+	    public static void addNew(int x, int y) {
+	        int sum = y;
+	        
+	        do {
+	        	sum = sum - 1;
+	        	x = x + 1;
+	        }while(x <= 1);
+	        
+	        do {
+	        	sum = sum + 1;
+	        	x = x - 1;
+	        }while(x > 0);
+	        
+	        System.out.println(sum);
+	    }
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sudheerpaturi
+ *
+ */
+module CSCM64 {
+}
